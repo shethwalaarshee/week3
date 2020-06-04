decd3bacfbcf5f00b011a03a3d6567f06ff132c3
User <User@192.168.0.27>
2020-06-04 09:31:47 -0400

    new repo

---
+++ src/syst/pkg17796/fundamentals/of/software/design/ThursdayExample.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package syst.pkg17796.fundamentals.of.software.design;
+
+/**
+ *
+ * @author User
+ */
+public class ThursdayExample {
+
+    public static void main(String[] args) {
+
+        System.out.println("learning pull operations");
+    }
+}
